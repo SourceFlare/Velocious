70ca9bc408af7556f4ff330290d979dd908760e3
Mark Greenall <mark@markgreenall.uk>
2016-06-21 17:14:24 +0100

    Create Exception.php

---
+++ core/Exception.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Borris\core;
+
+
+class Exception {
+    
+    /**
+     * Sets the status code required, then dies with the error.
+     */
+    public static function cast ($message, $response_code) {
+        http_response_code($response_code);
+        echo "Error $response_code.\n\n";
+        echo "$message";
+        die();
+    }
+}
