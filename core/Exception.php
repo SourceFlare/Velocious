122c3471bd697c607ce420e10cc01c5cef787f14
Mark Greenall <mark@markgreenall.uk>
2016-06-22 13:32:15 +0100

    Update Exception.php

--- core/Exception.php
+++ core/Exception.php
@@ -4,12 +4,14 @@ namespace Velocious\core;
 
 
 class Exception {
-    
     /**
      * Sets the status code required, then dies with the error.
+     * @param string $message
+     * @param int $response_code
      */
     public static function cast ($message, $response_code) {
         http_response_code($response_code);
+        header("Content-type: text/plain");
         echo "Error $response_code.\n\n";
         echo "$message";
         die();
