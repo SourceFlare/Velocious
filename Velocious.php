3427cc2ee5379d76358ad851e981054ebc61f81e
Mark Greenall <mark@markgreenall.uk>
2016-06-23 16:31:47 +0100

    Update Velocious.php

--- Velocious.php
+++ Velocious.php
@@ -11,5 +11,5 @@ require_once(__DIR__ . "/core/autoload.php");
 /**
  * Find Route and Prepare Variables
  */
-Router::find_route ($_REQUEST['url']);   # Find the route
-Router::execute();                       # Execute the route!
+$App = new Router ($_REQUEST['url']);
+$App->execute();
