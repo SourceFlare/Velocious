62b49f6626c66cda075084c55c82d21893047d7e
Mark Greenall <mark@markgreenall.uk>
2016-06-23 16:32:59 +0100

    Update Router.php

--- core/Router.php
+++ core/Router.php
@@ -2,6 +2,7 @@
 
 namespace Velocious\core;
 
+use Velocious\interfaces\RouterInterface;
 use Velocious\core\Render;
 use Velocious\core\MimeTypes;
 use Velocious\core\Exception;
@@ -10,8 +11,18 @@ use Velocious\core\Rules;
 
 class Router {
     
-    public static $state = [];
-    public static $route;
+    public $state = [];
+    public $route;
+    
+    /**
+     * Constructs the url into the class
+     * @param string $url
+     */
+    public function __construct (string $url) {
+    	if (!isset($url))
+    		Exception::cast("Error! No url was given.\n\nYour server rewriting configuration may be incorrect.\n\nPlease check and try again.", 500);
+    	$this->state['requested_url'] = $url;
+    }
     
     /**
      * Matches the URL against a defined route
@@ -19,7 +30,7 @@ class Router {
      * @param array $routes
      * @return bool
      */
-    public static function find_route (string $url) : bool {
+    final public function find_route () : Router {
     	
     	# You are using Velocious; be awesome!
     	header("X-Powered-By: Velocious!");
@@ -32,33 +43,30 @@ class Router {
         foreach ($route as $r => $obj) {
             
             # Get the preg_math patterns
-            $patterns = self::build_matching_patterns ($r);
+            $patterns = $this->build_matching_patterns ($r);
             
             # Run both patterns
             $result1 = preg_match($patterns['scalar_pattern'], $r,   $scalars);
-            $result2 = preg_match($patterns['url_pattern'],    $url, $parts);
+            $result2 = preg_match($patterns['url_pattern'],    $this->state['requested_url'], $parts);
             
             # Check that match ws ok
             if (!$result1 or !$result2)
                 continue;
             
-            # Build variables
-            $i=0;
-            foreach ($scalars as $s) {
-                self::$state[$s] = $parts[$i];
-                $i++;
-            }
+            # Set the state
+			$this->set_state($scalars, $parts);
             
             # Check for governance compliance
             Rules::govern($obj);
             
             # Put route into state
-            self::$route = $obj;
+            $this->route = $obj;
             
-            return true;
+            # Return the class 
+            # for other operations.
+            return $this;
         }
         Exception::cast("Could not find route.", 404);
-        return false;
     }
     
     /**
@@ -69,7 +77,7 @@ class Router {
      * @param string $abstracted_url
      * @return array
      */
-    final protected static function build_matching_patterns (string $abstracted_url) : array {
+    final protected function build_matching_patterns (string $abstracted_url) : array {
             # Build a pattern for the Route
             $url_pattern = '/^' . preg_replace(
                 "/{([a-zA-Z0-9\-\_\%\&\;]*)}/i", 
@@ -85,7 +93,7 @@ class Router {
             );
             
             return [
-                'url_pattern'      => $url_pattern,
+                'url_pattern'    => $url_pattern,
                 'scalar_pattern' => $scalar_pattern
             ];
     }
@@ -94,17 +102,50 @@ class Router {
      * Returns the Route's closure to be executed
      * @return object closure
      */
-    final public static function execute () {
-        
+    final public function execute () {
+    	
+    	# Locate the route, and instantiate the state
+    	$this->find_route();
+    	
+		# Check that the state is set
+		if (!isset($this->state) or empty($this->state))
+			Exception::cast ("Error! State has not been set!", 500);
+    	
         # Check that route is available
-        if(!isset(self::$route))
+        if(!isset($this->route))
             Exception::cast ("Could not execute route!", 500);
 		
         # Retrieve controller from route
-        $action = self::$route['Controller'];
+        $action = $this->route['Controller'];
             
         # Execute route!
-        $action(self::$state);
+        $action($this->state);
     }
     
+    
+    /**
+     * Sets the state object with the various vairbles
+     * such as URL matching, GET & POST, and others
+     * such as Superglobals (_SERVER, _SESSION, etc)
+     * @param array $scalars
+     * @return bool
+     */
+    final protected function set_state (array $scalars, array $parts) : bool {
+        # Put URL variables into the State
+        $i=0;
+        foreach ($scalars as $s) {                 # Variables from URL
+            $this->state[$s] = $parts[$i]; $i++; }
+        
+        # Put GET and POST variables into the state
+        foreach ($_REQUEST as $key => $value) {    # Variables from GET & POST
+            $this->state[$key] = $value; }
+        
+        # Reference super globals into State
+   	    $this->state['SERVER']  = &$_SERVER;
+   	    $this->state['FILES']   = &$_FILES;
+        $this->state['COOKIE']  = &$_COOKIE;
+  	    $this->state['SESSION'] = &$_SESSION;
+  	    
+  	    return true;
+    }
 }
