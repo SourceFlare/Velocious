da7a76dc1dfd6347a849770d3e9c487e98746ff8
Mark Greenall <mark@markgreenall.uk>
2016-06-23 11:14:30 +0100

    Update config.php

--- routes/config.php
+++ routes/config.php
@@ -7,39 +7,61 @@ use Velocious\core\MimeTypes;
 use Velocious\core\Exception;
 
 
-# Read a blog
-$route["/{page_id}/{blog_id}/"] = [
-
-	"Rules" => [
-		"Allowed_Request_Types" => ["POST", "GET"], 
-		"Allowed_Remote_Addr" => ["localhost", "::1"]],
-		
-	"Controller"  => function (array $state) : bool {
-	    return Render::json([
-	        'response' => [
-    	        'blog_page_id' => $state['page_id'],
-        	    'blog_id'      => $state['blog_id'],
-            	'blog_mode'    => $state['mode']
-	        ]
-	    ]);
-	}
+/**
+ * Index Route - Serves up a static HTML files from
+ * the ./html/ folder.
+ */
+$route["/{id}"] = [
+    
+    "Rules" => [
+        "Allowed_Request_Types" => ["GET"],
+        "Allowed_Remote_Addr"   => ["localhost", "::1"]],
+    
+    "Controller"  => function (array $state) : bool {
+        Render::file("./html/index.html", "html");
+        return true;
+    }
 ];
 
 
-# Delete a blog
-$route["/{page_id}/{blog_id}/delete/"] = [
+/**
+ * Read a blog using url variable matching. The
+ * variables in the url become available through
+ * the $state[] variable passed into the closure.
+ */
+$route["/blog/{page_id}/{blog_id}/"] = [
 
-	"Rules" => [
-		"Allowed_Request_Types" => ["POST"],
-		"Allowed_Remote_Addr"   => ["localhost", "::1"]],
-		
-	"Controller"  => function (array $state) : bool {
-	    return Render::json([
-	        'response' => [
-    	        'blog_page_id' => $state['page_id'],
-        	    'blog_id'      => $state['blog_id'],
-            	'blog_mode'    => $state['mode']
-	        ]
-	    ]);
-	}
+    "Rules" => [
+        "Allowed_Request_Types" => ["GET", "POST"], 
+        "Allowed_Remote_Addr" => ["localhost", "::1"]],
+        
+    "Controller"  => function (array $state) : bool {
+        return Render::json([
+            'response' => [
+                'blog_page_id' => $state['page_id'],
+                'blog_id'      => $state['blog_id']
+            ]
+        ]);
+    }
+];
+
+
+/**
+ * Now we use a service within a route to do 
+ * some grunt work and pass to the client.
+ */
+$route["/string-reverse/{string}/"] = [
+
+    "Rules" => [
+        "Allowed_Request_Types" => ["GET"],
+        "Allowed_Remote_Addr"   => ["localhost", "::1"]],
+        
+    "Controller"  => function (array $state) : bool {
+        return Render::json([
+            'response' => [
+                'string'          => $state['string'],
+                'reversed_string' => (new \Velocious\services\ReverseString)->commit($state['string'])
+            ]
+        ]);
+    }
 ];
